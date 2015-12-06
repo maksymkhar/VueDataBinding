b5923609f4cccad2a054d1524b458e754d9b6895
Max <maksymkharuk@iesebre.com>
2015-12-06 15:31:33 +0100

    Added content from jsbin

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
-/**
- * Created by max on 6/12/15.
- */
+Vue.component('alert', {
+
+    template: '#alert-template',
+
+    props: ['type'],
+
+    data: function()
+    {
+        return {
+            show : true
+        };
+    },
+    computed: {
+
+        alertClasses: function()
+        {
+            var type = this.type;
+
+            return {
+
+                'Alert': true,
+                'Alert--Success': type == 'success',
+                'Alert--Error': type == 'error'
+
+            };
+        }
+    }
+
+});
+
+new Vue({
+    el: 'body'
+});
